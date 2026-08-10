2958a185e792df97afc4cd5017db6c8cebf5b4bc
alaamando07-rgb <alaamando07@gmail.com>
2026-08-10 14:16:52 +0300

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -7,8 +7,8 @@ guests = cursor.fetchall()
 
 print("إليك روابط الضيوف جاهزة:")
 for name, token in guests:
-    # هذا الرابط هو الذي ستحوله إلى QR Code
-    link = f"http://127.0.0.1:5000/?token={token}"
+    # رابط الاستضافة السحابية المحدث
+    link = f"https://nmp-sy.onrender.com/?token={token}"
     print(f"الضيف: {name} | الرابط: {link}")
 
 conn.close()
